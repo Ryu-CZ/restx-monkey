15c18e58ccbdd6cd3a55b8c5c815c11939b81c06
Tomáš Trval <trval@kajot.cz>
2022-08-31 13:53:15 +0200

    add: python package setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name='restx_monkey',
+    install_requires=[
+        'flask-restx>=0.5',
+    ],
+)
