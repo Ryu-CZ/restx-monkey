78fafce4513107af4852d170fe74dd6392b090eb
Tomáš Trval <trval@kajot.cz>
2022-08-31 15:41:09 +0200

    modify: use pnly pyproject.oml for builds

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name='restx_monkey',
-    install_requires=[
-        'flask-restx>=0.5',
-    ],
-)
